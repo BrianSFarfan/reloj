82ec92d7b583d0da047a46fe01089f7a0bbcecd9
BrianSFarfan <brian.sfarfan@gmail.com>
2023-07-07 01:21:37 -0300

    Hola

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ var variableHora;
 var fechaActual;
 
 var horaYFecha;
-var variableFecha; 
+var variableFecha;
 
 var horaActual;
 
 function obtenerHora() {
   fechaActual = new Date();
 
-  var hora = fechaActual.getHours();
-  var minutos = fechaActual.getMinutes();
-  var segundos = fechaActual.getSeconds();
+  var hora = fechaActual.getHours().toString().padStart(2, '0'); // Agrega cero delante si tiene un solo dígito
+  var minutos = fechaActual.getMinutes().toString().padStart(2, '0'); // Agrega cero delante si tiene un solo dígito
+  var segundos = fechaActual.getSeconds().toString().padStart(2, '0'); // Agrega cero delante si tiene un solo dígito
 
   variableHora = hora + ":" + minutos + ":" + segundos;
 
@@ -21,11 +21,11 @@ function obtenerHora() {
 function obtenerFecha() {
   fechaActual = new Date();
 
-  var dia = fechaActual.getDate();
-  var mes = fechaActual.getMonth() + 1;
+  var dia = fechaActual.getDate().toString().padStart(2, '0'); // Agrega cero delante si tiene un solo dígito
+  var mes = (fechaActual.getMonth() + 1).toString().padStart(2, '0'); // Agrega cero delante si tiene un solo dígito
   var año = fechaActual.getFullYear();
 
-  variableFecha = dia + "/" + mes + "/" + año; 
+  variableFecha = dia + "/" + mes + "/" + año;
 
   return variableFecha;
 }
@@ -35,6 +35,7 @@ var variableHoraActual = document.getElementById("variableHoraActualCompleto");
 var variableFechaActual = document.getElementById("variableFechaActualCompleta");
 
 // Actualizar la hora y fecha cada segundo
+
 setInterval(function() {
   horaActual = obtenerHora();
   fechaActual = obtenerFecha();
