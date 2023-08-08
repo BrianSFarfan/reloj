562621e7c8eed6becf1e3171d88671612d348090
BrianSFarfan <brian.sfarfan@gmail.com>
2023-08-07 23:42:09 -0300

    a

--- fullScreen.js
+++ fullScreen.js
@@ -2,13 +2,19 @@ const fullscreenButton = document.getElementById('fullScreen');
 const fullscreenContent = document.querySelector('.fullBody');
 
 fullscreenButton.addEventListener('click', () => {
-  if (fullscreenContent.requestFullscreen) {
-    fullscreenContent.requestFullscreen();
-  } else if (fullscreenContent.mozRequestFullScreen) { // Para navegadores Firefox
-    fullscreenContent.mozRequestFullScreen();
-  } else if (fullscreenContent.webkitRequestFullscreen) { // Para navegadores WebKit (Chrome, Safari)
-    fullscreenContent.webkitRequestFullscreen();
-  } else if (fullscreenContent.msRequestFullscreen) { // Para Internet Explorer / Edge
-    fullscreenContent.msRequestFullscreen();
+  if (!document.fullscreenElement) {
+    fullscreenContent.classList.add('fullscreen-active');
+    if (fullscreenContent.requestFullscreen) {
+      fullscreenContent.requestFullscreen();
+    } else if (fullscreenContent.mozRequestFullScreen) {
+      fullscreenContent.mozRequestFullScreen();
+    } else if (fullscreenContent.webkitRequestFullscreen) {
+      fullscreenContent.webkitRequestFullscreen();
+    } else if (fullscreenContent.msRequestFullscreen) {
+      fullscreenContent.msRequestFullscreen();
+    }
+  } else {
+    document.exitFullscreen();
+    fullscreenContent.classList.remove('fullscreen-active');
   }
 });
